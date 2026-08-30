23df38ce4085af3087940dc690591a6ee2cc8a51
Ajay Rathour <ajayrathour8267@gmail.com>
2026-08-30 11:47:25 +0530

    feat: define sequelize associations for User, Project, Task, and RefreshToken models

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,22 @@ const RefreshToken = require('./RefreshToken');
 const Project = require('./Project');
 const Task = require('./Task');
 
+// User associations
+User.hasMany(Project, { foreignKey: 'userId', as: 'projects', onDelete: 'CASCADE' });
+User.hasMany(RefreshToken, { foreignKey: 'userId', as: 'refreshTokens', onDelete: 'CASCADE' });
+User.hasMany(Task, { foreignKey: 'assigneeId', as: 'assignedTasks', onDelete: 'SET NULL' });
+
+// Project associations
+Project.belongsTo(User, { foreignKey: 'userId', as: 'owner' });
+Project.hasMany(Task, { foreignKey: 'projectId', as: 'tasks', onDelete: 'CASCADE' });
+
+// Task associations
+Task.belongsTo(Project, { foreignKey: 'projectId', as: 'project' });
+Task.belongsTo(User, { foreignKey: 'assigneeId', as: 'assignee' });
+
+// RefreshToken associations
+RefreshToken.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
 module.exports = {
   User,
   RefreshToken,
